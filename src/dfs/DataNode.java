9d3626e4f40edbaa19e2e436e6ea5685f61c5e3a
stan4git <stan4g@gmail.com>
2014-07-03 01:55:25 -0400

    "updated datanode and node monitor"

--- src/dfs/DataNode.java
+++ src/dfs/DataNode.java
@@ -4,6 +4,7 @@ package dfs;
  * 2. Setup registry 
  * 3. receive file
  * 4. available chunk slot
+ * 5. makeCopy for RMI call
  */
 public class DataNode {
 
--- src/dfs/NodeMonitor.java
+++ src/dfs/NodeMonitor.java
@@ -2,9 +2,9 @@ package dfs;
 /**
  * 1. check heart beat of dataNode
  * 2. check replica amount
- * 3. 
- * 
- *
+ * 3. check write - confirm copy is made
+ * 4. assign to most available - mention in report
+ * 5. copy from next datanode
  */
 public class NodeMonitor {
 
