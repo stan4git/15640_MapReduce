126b58fb3e158e18786259ed26213f7c87f12ab7
stan4git <stan4g@gmail.com>
2014-07-03 01:59:33 -0400

    "updated node monitor"

--- src/dfs/NodeMonitor.java
+++ src/dfs/NodeMonitor.java
@@ -3,7 +3,7 @@ package dfs;
  * 1. check heart beat of dataNode
  * 2. check replica amount
  * 3. check write - confirm copy is made
- * 4. assign to most available - mention in report
+ * 4. assign to node with most available - mention in report
  * 5. copy from next datanode
  */
 public class NodeMonitor {
