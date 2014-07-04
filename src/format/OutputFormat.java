f9a1c3cd58573c05d6a067df2e93a97307a76d81
cater1986 <cater232678@gmail.com>
2014-07-04 16:00:36 -0400

    outputFormat

---
+++ src/format/OutputFormat.java
@@ -0,0 +1,37 @@
+package format;
+
+import java.util.ArrayList;
+import java.util.SortedMap;
+
+/**
+ * This class is used to format the OutputCollector object to a string. This
+ * string has a format "key value \n"
+ * 
+ * @author menglonghe
+ * @author sidilin
+ * 
+ */
+public class OutputFormat {
+
+	/**
+	 * This method is actually to format the OutputCollector object to a
+	 * string.This string has a format "key value \n"
+	 * 
+	 * @param collector
+	 *            this is a OutputCollector object
+	 * @return String
+	 */
+	public String formatOutput(OutputCollector collector) {
+		StringBuffer sb = new StringBuffer("");
+		SortedMap<Object, ArrayList<Object>> outputMap = collector.outputCollector;
+		for (Object key : outputMap.keySet()) {
+			for (Object value : outputMap.get(key)) {
+				sb.append(key);
+				sb.append(" ");
+				sb.append(value);
+				sb.append("\n");
+			}
+		}
+		return sb.toString();
+	}
+}
