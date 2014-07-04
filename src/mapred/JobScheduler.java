25256a24a45af21f1fc5052f4f359f9c6bdae750
stan4git <stan4g@gmail.com>
2014-07-04 01:06:30 -0400

    "added jobScheduler"

---
+++ src/mapred/JobScheduler.java
@@ -0,0 +1,8 @@
+package mapred;
+/**
+ * 1. decide mapper (return a list)
+ * 2. pickBestNode() - local/global weight * available slots
+ */
+public class JobScheduler {
+
+}
--- src/mapred/JobTracker.java
+++ src/mapred/JobTracker.java
@@ -1,7 +1,6 @@
 package mapred;
 /**
  * 1. submit job
- * 2. pickBestNode() - local/global weight * available slots
  */
 public class JobTracker {
 
