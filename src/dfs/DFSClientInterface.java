c0be0b2a9744a17430614ec757db1dc57b721aa5
Yijie Zhuang <jes2icazhuang@gmail.com>
2014-07-12 20:34:12 -0400

    solve a shitty problem

--- src/dfs/DFSClientInterface.java
+++ src/dfs/DFSClientInterface.java
@@ -1,8 +1,8 @@
 package dfs;
 
-import java.io.Serializable;
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 public interface DFSClientInterface extends Remote {
-	public void sendChunkReceivedACK(String fromIP, String filename, int chunkNum);
+	public void sendChunkReceivedACK(String fromIP, String filename, int chunkNum) throws RemoteException;
 }
