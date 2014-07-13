d86ec9cdab85d181c16e949165a708dadefeebc6
cater1986 <cater232678@gmail.com>
2014-07-13 19:56:51 -0400

    haha

--- src/util/FunctionalUtil.java
+++ src/util/FunctionalUtil.java
@@ -2,12 +2,11 @@ package util;
 
 import java.util.HashSet;
 import java.util.Hashtable;
-import java.util.Timer;
-import java.util.TimerTask;
 import java.util.Map.Entry;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class FunctionalUtil {
+	
 	public static <K1, K2, V> ConcurrentHashMap<K1, Hashtable<K2, HashSet<V>>> deepCopy(
 			ConcurrentHashMap<K1, Hashtable<K2, HashSet<V>>> original) {
 		ConcurrentHashMap<K1, Hashtable<K2, HashSet<V>>> copy = new ConcurrentHashMap<K1, Hashtable<K2, HashSet<V>>>();
@@ -31,17 +30,4 @@ public class FunctionalUtil {
 		}
 		return copySet;
 	}
-	
-	public void timeToClose() {
-		Timer timer = new Timer();
-		timer.schedule(new RemindTask(), 1 * 1000);
-	}
-	
-	class RemindTask extends TimerTask {
-	    public void run() {
-	      System.out.println("Time's up!");
-	      //timer.cancel(); //Not necessary because we call System.exit
-	      System.exit(0); //Stops the AWT thread (and everything else)
-	    }
-	  }
 }
