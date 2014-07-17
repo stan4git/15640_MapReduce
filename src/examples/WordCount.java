2633a881a4540dcec8ffe5e770fd202c3ce01ecf
cater1986 <cater232678@gmail.com>
2014-07-16 22:47:14 -0400

    dddd

--- src/examples/WordCount.java
+++ src/examples/WordCount.java
@@ -10,7 +10,7 @@ public class WordCount {
 
 	public static void main(String[] args) {
 		JobConfiguration conf = new JobConfiguration();
-		conf.setInputfile("input.txt");
+		conf.setInputfile("pg_00");
 		conf.setOutputfile("output");
 		conf.setInputFormat(WordFormat.class);
 		conf.setOutputFormat(OutputFormat.class);
